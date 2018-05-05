dfa7d3ad78ca801d05c76df31aca8d981f586d35
ryuchun00 <leokun0210@gmail.com>
2018-05-05 17:22:25 +0900

    パラメーター付きのshop.jsonは未検証だったので検証した

--- spec/colorme_public_api/endpoint/shop_spec.rb
+++ spec/colorme_public_api/endpoint/shop_spec.rb
@@ -2,17 +2,37 @@ describe ColormePublicApi::Endpoint::V1::Shop do
   describe '#get' do
     let(:client) { ColormePublicApi::Client.new(build_config) }
 
-    context 'no paramater' do
+    context 'no parameter' do
       subject (:response) { client.shop.get(access_token: test_access_token) }
 
       before do
-        VCR.use_cassette('endpoint/v1/shop/get_success') do
+        VCR.use_cassette('endpoint/v1/shop/no_parameter_success') do
           response
         end
       end
 
       it 'succeeds' do
         expect(response['shop'].is_a?(Hash)).to be true
+      end
+    end
+
+    context 'have parameters' do
+      let(:params) { {
+        fields: 'id,domain_plan'
+      } }
+
+      subject (:response) { client.shop.get(access_token: test_access_token, params: params) }
+
+      before do
+        VCR.use_cassette('endpoint/v1/shop/have_parameters_success') do
+          response
+        end
+      end
+
+      it 'succeeds' do
+        expect(response['shop']['id'].is_a?(String)).to be true
+        expect(response['shop']['domain_plan'].is_a?(String)).to be true
+        expect(response['shop']['state'].nil?).to be true
       end
     end
   end
